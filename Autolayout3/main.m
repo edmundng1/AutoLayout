765809e96f86ca3704a48d446e7fa0a391376bc0
Edmund <pawswee@gmail.com>
2016-05-13 11:51:14 -0700

    first commit

---
+++ Autolayout3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Autolayout3
+//
+//  Created by Edmund Ng on 2016-05-10.
+//  Copyright © 2016 paperchasers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
